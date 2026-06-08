3988a94311b1c049226d74c33be8b98462d74b59
Pinal Naik <pinal.naik@gmail.com>
2026-06-08 17:52:40 +0530

    adjusted spaces

--- 01-introduction-buttons-actions/Starter/Bullseye/Bullseye/BullseyeApp.swift
+++ 01-introduction-buttons-actions/Starter/Bullseye/Bullseye/BullseyeApp.swift
@@ -1,12 +1,10 @@
-//
-
 import SwiftUI
 
 @main
 struct BullseyeApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
 }
